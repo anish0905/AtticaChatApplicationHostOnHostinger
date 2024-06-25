13cbeee81214326d183d19448c42f38fbee171d2
sangeeta586 <sangeetasahu241999@gmail.com>
2024-06-25 17:18:05 +0530

    updated

--- frontend/src/components/employee/EmployeeSidebar.jsx
+++ frontend/src/components/employee/EmployeeSidebar.jsx
@@ -26,27 +26,29 @@ const EmployeeSidebar = () => {
   const isActive = (path) => location.pathname === path;
 
   return (
-    <div className='flex flex-row lg:flex-col h-[80px] lg:h-screen w-full lg:w-[140px] left-0 bg-[#5443c3] border-b lg:border-r shadow-md justify-between items-center py-[10px] lg:py-[20px] text-gray-500 text-2xl md:text-3xl'>
-      <div className="w-16 md:w-24 lg:w-32 h-16 md:h-16  lg:h-24 mx-3 bg-[#fffefd] rounded-2xl flex items-center justify-center">
-        <img className="m-2 md:m-4 lg:m-10" src={logo} alt="Logo" />
+    <div className="flex flex-row lg:flex-col h-[80px] lg:h-screen w-full lg:w-[100px] left-0 bg-[#5443c3] border-b lg:border-r shadow-md justify-between items-center py-[10px] lg:py-[20px] text-gray-500">
+      <div className="w-16 md:w-20 lg:w-24 h-12 md:h-16 lg:h-20 mx-3 bg-[#fffefd] rounded-2xl flex items-center justify-center">
+        <img className="m-2 md:m-4 lg:m-6" src={logo} alt="Logo" />
       </div>
       
-      <div className="flex flex-row lg:flex-col gap-[10px] sm:gap-[10px] md:gap-[10px] lg:gap-[40px] relative">
+      <div className="flex flex-row lg:flex-col gap-2 md:gap-3 lg:gap-5 relative">
         <div 
           onClick={handleGroup} 
           className={`group relative flex items-center rounded-full p-3 md:p-5 ${isActive("/empgroupchat") ? "bg-blue-500 text-white" : "bg-[#fffefd]"}`}
         >
-          <GrChatOption />
+          <GrChatOption className="text-lg md:text-2xl lg:text-3xl"/>
           <span className="absolute top-full lg:top-auto lg:left-full ml-2 lg:ml-0 lg:mt-2 lg:mb-0 whitespace-nowrap bg-white text-black text-xs md:text-sm rounded py-1 px-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
             Group Chat
           </span>
         </div>
+
         <div 
           onClick={handleChat} 
           className={`group relative flex items-center rounded-full p-3 md:p-5 ${isActive("/chat") ? "bg-blue-500 text-white" : "bg-[#fffefd]"}`}
         >
-          <BsChatSquareDots />
-          <span className="absolute top-full lg:top-auto lg:left-full ml-2 lg:ml-0 lg:mt-2 lg:mb-0 whitespace-nowrap bg-white text-black text-xs md:text-sm rounded py-1 px-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+
+          <BsChatSquareDots className="text-lg md:text-2xl lg:text-3xl" />
+          <span className="absolute lg:bottom-auto lg:left-full mt-16 lg:ml-0 lg:mt-2 whitespace-nowrap z-50 bg-black text-white text-xs md:text-sm rounded py-1 px-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
             Chat
           </span>
         </div>
@@ -55,8 +57,9 @@ const EmployeeSidebar = () => {
           to="/empToadmin" 
           className={`group relative flex items-center rounded-full p-3 md:p-5 ${isActive("/empToadmin") ? "bg-blue-500 text-white" : "bg-[#fffefd]"}`}
         >
-          <RiContactsLine />
-          <span className="absolute top-full lg:top-auto lg:left-full ml-2 lg:ml-0 lg:mt-2 lg:mb-0 whitespace-nowrap bg-white text-black text-xs md:text-sm rounded py-1 px-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+
+          <RiContactsLine className="text-lg md:text-2xl lg:text-3xl"/>
+          <span className="absolute lg:bottom-auto lg:left-full mt-16 lg:ml-0 lg:mt-2 whitespace-nowrap z-50 bg-black text-white text-xs md:text-sm rounded py-1 px-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
             Admin
           </span>
         </Link>
@@ -64,8 +67,8 @@ const EmployeeSidebar = () => {
       </div>
       
       <div onClick={handleLogout} className="group relative flex items-center bg-[#fffefd] rounded-full p-3 md:p-5 ">
-        <BiLogOut />
-        <span className="absolute top-full lg:top-auto lg:left-full ml-2 lg:ml-0 lg:mt-2 lg:mb-0 whitespace-nowrap bg-white text-black text-xs md:text-sm rounded py-1 px-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+        <BiLogOut className="text-lg md:text-2xl lg:text-3xl"/>
+        <span className="absolute lg:bottom-auto lg:left-full mt-16 lg:ml-0 lg:mt-2 whitespace-nowrap bg-black text-white text-xs md:text-sm rounded py-1 px-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
           Logout
         </span>
       </div>
