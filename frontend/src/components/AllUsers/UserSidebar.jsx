d055ff8069895bc7a98ef94cbba3c8a93bfc4708
anish0905 <anishanish0102@gmail.com>
2024-07-08 14:33:54 +0530

    te chat completed

--- frontend/src/components/AllUsers/UserSidebar.jsx
+++ frontend/src/components/AllUsers/UserSidebar.jsx
@@ -1,11 +1,10 @@
 import { BiLogOut } from "react-icons/bi";
 import { BsChatSquareDots } from "react-icons/bs";
 import { RiContactsLine } from "react-icons/ri";
-import { useNavigate, useLocation } from 'react-router-dom';
+import { useNavigate, useLocation } from "react-router-dom";
 import logo from "../../assests/logo.png";
 import { CgProfile } from "react-icons/cg";
-import {  useState } from "react";
-
+import { useState } from "react";
 
 const UserSidebar = ({ value }) => {
   const userDetails = JSON.parse(localStorage.getItem("userDetails"));
@@ -41,6 +40,10 @@ const UserSidebar = ({ value }) => {
       case "SECURITY":
         navigate("/SecurityChat");
         break;
+      case "TE":
+        navigate("/TEChat");
+        break;
+
       default:
         navigate("/");
     }
@@ -85,13 +88,13 @@ const UserSidebar = ({ value }) => {
   const isActive = (path) => location.pathname === path;
 
   return (
-    <div className='flex flex-row lg:flex-col h-[80px] lg:h-screen w-full lg:w-[140px] left-0 bg-[#5443c3] border-b lg:border-r shadow-md justify-between items-center py-[10px] lg:py-[20px] text-gray-500 text-2xl md:text-3xl'>
+    <div className="flex flex-row lg:flex-col h-[80px] lg:h-screen w-full lg:w-[140px] left-0 bg-[#5443c3] border-b lg:border-r shadow-md justify-between items-center py-[10px] lg:py-[20px] text-gray-500 text-2xl md:text-3xl">
       <div className="w-16 md:w-24 lg:w-32 h-16 md:h-16 lg:h-24 mx-3 bg-[#fffefd] rounded-2xl flex items-center justify-center">
         <img className="m-2 md:m-4 lg:m-10" src={logo} alt="Logo" />
       </div>
-      <p className='text-white text-base lg:text-xl'>{userDetails?.name}</p> 
-   
-    {/* __________________________user Details__________________ */}
+      <p className="text-white text-base lg:text-xl">{userDetails?.name}</p>
+
+      {/* __________________________user Details__________________ */}
 
       <div className=" group relative flex items-center bg-[#fffefd] rounded-full p-3 md:p-5 ">
         <div
@@ -99,12 +102,11 @@ const UserSidebar = ({ value }) => {
           onMouseEnter={() => setShowTooltip(true)}
           onMouseLeave={() => setShowTooltip(false)}
         >
-        
           <CgProfile />
-      
+
           <span className="absolute top-full lg:top-auto lg:left-full ml-2 lg:ml-0 lg:mt-2 lg:mb-0 whitespace-nowrap bg-white text-black text-xs md:text-sm rounded py-1 px-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-          Profile
-        </span>
+            Profile
+          </span>
           {showTooltip && (
             <div className="absolute top-full left-0 mt-2  p-4 bg-white text-black text-sm md:text-md rounded shadow-md z-10 items-start">
               <p>Email: {userDetails.email}</p>
@@ -113,17 +115,16 @@ const UserSidebar = ({ value }) => {
             </div>
           )}
         </div>
-        
       </div>
-    
 
-
-{/* _______________________________________//________________________________________ */}
+      {/* _______________________________________//________________________________________ */}
 
       <div className="flex flex-row lg:flex-col gap-[10px] sm:gap-[10px] md:gap-[10px] lg:gap-[40px] relative">
-        <div 
-          onClick={handleChat} 
-          className={`group relative flex items-center rounded-full p-3 md:p-5 ${isActive("/chat") ? "bg-blue-500 text-white" : "bg-[#fffefd]"}`}
+        <div
+          onClick={handleChat}
+          className={`group relative flex items-center rounded-full p-3 md:p-5 ${
+            isActive("/chat") ? "bg-blue-500 text-white" : "bg-[#fffefd]"
+          }`}
         >
           <BsChatSquareDots />
           <span className="absolute top-full lg:top-auto lg:left-full ml-2 lg:ml-0 lg:mt-2 lg:mb-0 whitespace-nowrap bg-white text-black text-xs md:text-sm rounded py-1 px-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
@@ -131,22 +132,23 @@ const UserSidebar = ({ value }) => {
           </span>
         </div>
 
-        <div 
-          onClick={handleAdminChat} 
-          className={`group relative flex items-center rounded-full p-3 md:p-5 ${isActive("/chat") ? "bg-blue-500 text-white" : "bg-[#fffefd]"}`}
+        <div
+          onClick={handleAdminChat}
+          className={`group relative flex items-center rounded-full p-3 md:p-5 ${
+            isActive("/chat") ? "bg-blue-500 text-white" : "bg-[#fffefd]"
+          }`}
         >
-          <RiContactsLine/>
+          <RiContactsLine />
           <span className="absolute top-full lg:top-auto lg:left-full ml-2 lg:ml-0 lg:mt-2 lg:mb-0 whitespace-nowrap bg-white text-black text-xs md:text-sm rounded py-1 px-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
             Admin
           </span>
         </div>
       </div>
-      
-    
 
-
-
-      <div onClick={handleLogout} className="group relative flex items-center bg-[#fffefd] rounded-full p-3 md:p-5 ">
+      <div
+        onClick={handleLogout}
+        className="group relative flex items-center bg-[#fffefd] rounded-full p-3 md:p-5 "
+      >
         <BiLogOut />
         <span className="absolute top-full lg:top-auto lg:left-full ml-2 lg:ml-0 lg:mt-2 lg:mb-0 whitespace-nowrap bg-white text-black text-xs md:text-sm rounded py-1 px-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
           Logout
@@ -157,5 +159,3 @@ const UserSidebar = ({ value }) => {
 };
 
 export default UserSidebar;
-
-
---
+++ frontend/src/components/admin2/AdminLogin1.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import babusir from "../../assests/babusirr.png";
+import back3 from "../../assests/back3.png";
+
+const AdminLogin1 = () => {
+  return (
+    <div
+      className=" min-h-screen bg-cover "
+      style={{ backgroundImage: `url(${back3})` }}
+    >
+      <h1>
+        jhujhijloren lorem 50 Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Beatae consectetur commodi quos impedit totam, rem molestias!
+        Nesciunt officia, exercitationem amet laborum adipisci nobis dolore
+        dolorum delectus obcaecati deleniti provident reprehenderit maxime!
+        Aperiam ex deserunt error nostrum minus vero dolores debitis blanditiis
+        possimus delectus. Perferendis laudantium et eaque, asperiores quasi
+        excepturi!
+      </h1>
+    </div>
+  );
+};
+
+export default AdminLogin1;
+
+// import React from "react";
+// import back3 from "../../assests/back3.png"; // Make sure to adjust the path to your image
+
+// const AdminLogin1 = () => {
+//   return (
+//     <div className="" style={{ backgroundImage: `url(${back3})` }}>
+//       {/* Add any additional content or components here */}
+//     </div>
+//   );
+// };
+
+// export default AdminLogin1;
