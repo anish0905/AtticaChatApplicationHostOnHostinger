87af4e30693e5f7717373585520e23e2e084490d
anish0905 <anishanish0102@gmail.com>
2024-07-09 12:03:40 +0530

    added location Te

--- server/src/routes/locationRoute.js
+++ server/src/routes/locationRoute.js
@@ -1,9 +1,19 @@
 // locationRoutes.js
 const express = require("express");
 const router = express.Router();
-const { saveLocation,getLocation } = require("../controllers/locationController");
+const {
+  saveLocation,
+  getLocation,
+  TeSaveLocation,
+  TeGetLocationByDate,
+} = require("../controllers/locationController");
 
 // Save location
 router.post("/", saveLocation);
 router.get("/get/:id", getLocation);
+
+router.post("/te-location", TeSaveLocation);
+// Route to get location data by date
+router.get("/get-location/:userId/:date", TeGetLocationByDate);
+
 module.exports = router;
