7b42dc9f17ed8a0917c90052539a3549d95849af
lavanya00123 <lavanyabbasavaraj@gmail.com>
2024-07-20 15:08:08 +0530

    chat

--- frontend/src/components/employee/ChatPage.jsx
+++ frontend/src/components/employee/ChatPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
-import { AiOutlineSearch ,AiOutlineDown} from "react-icons/ai";
+import { AiOutlineSearch, AiOutlineDown } from "react-icons/ai";
 import { IoIosDocument } from "react-icons/io";
 import EmployeeSidebar from "./EmployeeSidebar";
 import { BASE_URL } from "../../constants";
@@ -10,7 +10,7 @@ import AllUsersFileModel from "../AllUsers/AllUsersFileModel";
 import { FaArrowLeft, FaCamera } from "react-icons/fa";
 import { IoMdSend } from "react-icons/io";
 import Camera from "../Camera/Camera";
-import ScrollingNavbar from "../admin/ScrollingNavbar";  
+import ScrollingNavbar from "../admin/ScrollingNavbar";
 
 function ChatPage() {
   const [messages, setMessages] = useState([]);
@@ -74,13 +74,12 @@ function ChatPage() {
     return () => clearInterval(intervalId);
   }, [sender, recipient]);
 
-
-  const handleSendMessage =async () => {
+  const handleSendMessage = async () => {
     if (!newMessage.trim() && !attachment) return;
 
     const messageData = {
       sender: loggedInUserId,
-      senderName:userDetails.name,
+      senderName: userDetails.name,
       recipient: recipient,
       text: newMessage,
       image: attachment?.type.startsWith("image/") ? attachment.url : null,
@@ -90,18 +89,14 @@ function ChatPage() {
       video: attachment?.type.startsWith("video/") ? attachment.url : null,
     };
 
-    
     try {
-      const response= await axios.post(`${BASE_URL}/api/postmessages`,messageData)
+      const response = await axios.post(`${BASE_URL}/api/postmessages`, messageData);
       setMessages([...messages, response.data.data]);
       setNewMessage("");
       setAttachment(null);
     } catch (error) {
-      console.log(error)
-      
+      console.log(error);
     }
-
-    
   };
 
   const handleFileUpload = (file) => {
@@ -122,8 +117,8 @@ function ChatPage() {
           const unreadUsersData = await Promise.all(
             users.map(async (user) => {
               const response = await axios.get(
-               `${BASE_URL}/api/mark-messages-read/${user._id}
-              `);
+                `${BASE_URL}/api/mark-messages-read/${user._id}`
+              );
               return { userId: user._id, data: response.data };
             })
           );
@@ -133,11 +128,8 @@ function ChatPage() {
         }
       };
       fetchUnreadMessages();
-      // const intervalId = setInterval(fetchUnreadMessages, 3 * 1000);
-      // return () => clearInterval(intervalId);
     }
   }, [users]);
-
 
   const handleBackToEmployees = () => {
     setShowChat(false);
@@ -174,14 +166,12 @@ function ChatPage() {
   };
 
   const handleForward = (message) => {
-    console.log(message);
     setForwardMessage(message);
     setShowForwardModal(true);
     setShowDropdown(null);
   };
 
   const handleForwardMessage = () => {
-
     setShowForwardModal(false);
     setShowDropdown(null);
   };
@@ -199,28 +189,25 @@ function ChatPage() {
     setShowCamera(false);
   };
 
-
   return (
     <div className="flex flex-col lg:flex-row h-screen overflow-hidden">
-      <ScrollingNavbar  /> 
+      <ScrollingNavbar />
       <EmployeeSidebar />
-      
+
       {showChat ? (
         <div className="w-full mb-20 lg:mb-0 flex flex-col justify-between overflow-hidden">
           <div className="flex items-center justify-between p-4 lg:bg-[#5443c3] lg:text-white text-[#5443c3] bg-white sticky top-0 z-10">
-          <button
+            <button
               onClick={handleBackToEmployees}
-              className="  lg:text-2xl p-2 rounded-md lg:bg-[#5443c3] lg:text-white text-[#5443c3] bg-white"
+              className="lg:text-2xl p-2 rounded-md lg:bg-[#5443c3] lg:text-white text-[#5443c3] bg-white"
             >
               <FaArrowLeft />
             </button>
-          
-              <h1 className="lg:text-2xl text-xl font-bold">{recipientName}</h1>
-            
-          
+
+            <h1 className="lg:text-2xl text-xl font-bold">{recipientName}</h1>
           </div>
           <div className="flex-grow overflow-y-auto p-4 flex flex-col h-screen bg-[#eef2fa]">
-            {messages.map((message,index) => (
+            {messages.map((message, index) => (
               <div
                 key={message._id}
                 className={`mb-4 p-4 rounded-lg max-w-[50%] relative break-words whitespace-pre-wrap lg:text-3xl md:text-xl text-sm font-bold ${
@@ -228,11 +215,10 @@ function ChatPage() {
                     ? "bg-[#e1dff3] text-[#5443c3] border border-[#5443c3] rounded-tr-3xl rounded-bl-3xl self-end"
                     : "bg-white text-[#5443c3] border border-[#5443c3] rounded-tl-3xl rounded-br-3xl self-start"
                 }`}
-
                 onMouseEnter={() => handleHover(index)}
                 onMouseLeave={() => setHoveredMessage(null)}
-              >  
-               {message.content && message.content.originalMessage && (
+              >
+                {message.content && message.content.originalMessage && (
                   <div className="mb-2">
                     <span className="bg-green-300 px-2 py-1 text-xs text-white rounded">
                       {message.content.originalMessage}
@@ -240,7 +226,9 @@ function ChatPage() {
                   </div>
                 )}
                 {message.content && message.content.text && (
-                  <p className="font-medium lg:text-xl md:text-xl text-sm ">{message.content.text}</p>
+                  <p className="font-medium lg:text-xl md:text-xl text-sm">
+                    {message.content.text}
+                  </p>
                 )}
                 {message.content && message.content.image && (
                   <img
@@ -249,11 +237,11 @@ function ChatPage() {
                     className="rounded-lg lg:h-96 lg:w-72 md:h-96 md:w-64 h-40 w-32 my-2"
                   />
                 )}
-                 {message.content && message.content.camera && (
+                {message.content && message.content.camera && (
                   <img
                     src={message.content.camera}
-                    alt="Image"
-                    className="rounded-lg lg:h-96 lg:w-72 md:h-96 md:w-64 h-40 w-32"
+                    alt="Camera"
+                    className="rounded-lg lg:h-96 lg:w-72 md:h-96 md:w-64 h-40 w-32 my-2"
                   />
                 )}
                 {message.content && message.content.document && (
@@ -267,7 +255,10 @@ function ChatPage() {
                   </a>
                 )}
                 {message.content && message.content.video && (
-                  <video controls className="max-w-xs lg:h-96 lg:w-72 md:h-96 md:w-64 h-40 w-32 text-4xl">
+                  <video
+                    controls
+                    className="max-w-xs lg:h-96 lg:w-72 md:h-96 md:w-64 h-40 w-32 text-4xl"
+                  >
                     <source src={message.content.video} type="video/mp4" />
                     Your browser does not support the video tag.
                   </video>
@@ -275,78 +266,88 @@ function ChatPage() {
                 <span className="text-xs text-gray-500">
                   {new Date(message.createdAt).toLocaleString()}
                 </span>
-              
+
                 {hoveredMessage === index && (
-                    <AiOutlineDown
-                      className="absolute top-2 right-2 cursor-pointer"
-                      onClick={() => handleDropdownClick(index)}
-                    />
-                  )}
-            
-                  {showDropdown === index && (
-                    <div className="absolute top-8 right-2 bg-white  rounded shadow-lg z-10">
-                      <button
-                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                        onClick={() => handleReply(message)}
-                      >
-                        Reply
-                      </button>
-                      <button
-                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                        onClick={() => handleForward(message)}
-                      >
-                        Forward
-                      </button>
-                    </div>
-                  )}
+                  <AiOutlineDown
+                    className="absolute top-2 right-2 cursor-pointer"
+                    onClick={() => handleDropdownClick(index)}
+                  />
+                )}
+
+                {showDropdown === index && (
+                  <div className="absolute top-8 right-2 bg-white rounded shadow-lg z-10">
+                    <button
+                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                      onClick={() => handleReply(message)}
+                    >
+                      Reply
+                    </button>
+                    <button
+                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
+                      onClick={() => handleForward(message)}
+                    >
+                      Forward
+                    </button>
+                  </div>
+                )}
               </div>
             ))}
             <div ref={messagesEndRef} />
             {showCamera && (
               <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-75">
-                <Camera onCapture={handleCapture} onClose={handleCloseCamera} loggedInUserId={loggedInUserId} recipient={recipient} />
+                <Camera
+                  onCapture={handleCapture}
+                  onClose={handleCloseCamera}
+                  loggedInUserId={loggedInUserId}
+                  recipient={recipient}
+                />
               </div>
             )}
           </div>
 
-          <div className="flex items-center p-4 bg-[#eef2fa] border-t border-gray-200 fixed bottom-0 w-full lg:static">
+          <div className="flex items-center p-4 bg-[#eef2fa] border-t border-gray-200 fixed bottom-0 w-full lg:static ">
             <input
               type="text"
               value={newMessage}
               onChange={(e) => setNewMessage(e.target.value)}
               placeholder="Type a message..."
-               className="flex-grow p-2 border rounded-lg mr-2 border-[#5443c3]"
+              className="flex-grow p-2 border rounded-lg mr-2 border-[#5443c3]"
             />
-             <button
-              onClick={() => setShowCamera(true)}
-              className="mr-2 text-xl"
-            >
+            <button onClick={() => setShowCamera(true)} className="mr-2 text-xl">
               <FaCamera />
             </button>
-           
+
             <button
               onClick={handleSendMessage}
-               className="bg-[#5443c3] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              className="bg-[#5443c3] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
             >
               <IoMdSend />
             </button>
-            <AllUsersFileModel sender={loggedInUserId} recipient={recipient} senderName={userDetails.name} />
+            <AllUsersFileModel
+              sender={loggedInUserId}
+              recipient={recipient}
+              senderName={userDetails.name}
+            />
           </div>
         </div>
       ) : (
-        <div className="w-full lg:w-1/4 bg-white p-4 overflow-y-auto">
-             <h1 className="lg:text-2xl text-xl font-bold mb-4 text-[#5443c3] lg:m-4  ">All Employees</h1>
-          <div className=" relative flex items-center mb-4 ">
-            <input
-              type="text"
-              value={userSearchQuery}
-              onChange={(e) => setUserSearchQuery(e.target.value)}
-              placeholder="Search by names..."
-              className="w-full h-10 p-2 text-base text-gray-700 rounded-xl pl-10 bg-white border border-[#5443c3] shadow-lg "
-            />
-         <AiOutlineSearch className="absolute top-3 left-3 text-gray-500 text-2xl" />
+        <div className="w-full lg:w-1/4 bg-white p-4 overflow-y-auto sticky mt-10 ">
+          <div className="sticky top-0 bg-white  z-10">
+            <h1 className="lg:text-2xl text-xl font-bold mb-4 text-[#5443c3] lg:m-4">
+              All Employees
+            </h1>
+            <div className="relative flex items-center mb-5">
+              <input
+                type="text"
+                value={userSearchQuery}
+                onChange={(e) => setUserSearchQuery(e.target.value)}
+                placeholder="Search by names..."
+                className="w-full h-10 p-2 text-base text-gray-700 rounded-xl pl-10 bg-white border border-[#5443c3] shadow-lg"
+              />
+              <AiOutlineSearch className="absolute top-3 left-3 text-gray-500 text-2xl" />
+            </div>
           </div>
-          <ul>
+          <ul className="overflow-y-auto h-[87%] "> {/* Set a fixed height for the user list */}
             {users
               .filter((user) =>
                 user.name.toLowerCase().includes(userSearchQuery.toLowerCase())
@@ -355,9 +356,7 @@ function ChatPage() {
                 <li
                   key={user._id}
                   className={`p-4 mb-2 rounded-lg cursor-pointer flex justify-between text-[#5443c3] text-sm font-medium ${
-                    unreadUsers.some(
-                      (unreadUser) => unreadUser.userId === user._id
-                    )
+                    unreadUsers.some((unreadUser) => unreadUser.userId === user._id)
                       ? "bg-blue-200"
                       : "bg-gray-200"
                   } ${recipient === user._id ? "bg-green-200" : ""}`}
@@ -375,10 +374,8 @@ function ChatPage() {
         </div>
       )}
 
-
-
-{showForwardModal && (
-        <ForwardModalAllUsers   
+      {showForwardModal && (
+        <ForwardModalAllUsers
           users={users}
           forwardMessage={forwardMessage}
           onForward={handleForwardMessage}
@@ -392,7 +389,6 @@ function ChatPage() {
           recipient={recipient}
           isVisible={showReplyModal}
           onClose={() => setShowReplyModal(false)}
-
         />
       )}
     </div>
